3bdb870d599024b150e2d5579e052f5fd42b99c4
lukmanov1987 <lukmanovim1987@gmail.com>
2026-04-01 19:45:33 +0300

    fix: удалил ненужные зависимости

--- utils/graphs.py
+++ utils/graphs.py
@@ -1,15 +1,9 @@
 import pandas as pd
 import numpy as np
 
-import plotly.graph_objects as go
-import plotly.express as px
-from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
 
 from typing import List, Optional, Dict, Union, Tuple
-
-
-
 
 def plot_funnel(data: pd.DataFrame,
                 stages: List[str],                
